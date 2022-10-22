27de4536c344b508745c8f724e9f6541476042bd
hongweihsu <hongweih@student.unimelb.edu.au>
2022-10-22 13:02:29 +1100

    basic layout

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {RecipeObj} from "../recipe.model";
 
 @Component({
   selector: 'app-recipes-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
+  recipes: RecipeObj[] = [
+    new RecipeObj('testRecipe','a simple test', 'https://images.immediate.co.uk/production/volatile/sites/30/2013/05/Puttanesca-fd5810c.jpg?quality=90&webp=true&resize=600,545'),
+    new RecipeObj('testRecipe','a simple test', 'https://images.immediate.co.uk/production/volatile/sites/30/2013/05/Puttanesca-fd5810c.jpg?quality=90&webp=true&resize=600,545')
+  ];
 
   constructor() { }
 
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {IngredientObj} from "../recipes/shared/ingredient.model";
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients: IngredientObj[] = [
+    new IngredientObj('Apples', 5),
+    new IngredientObj('Tomato', 3),
+  ];
 
   constructor() { }
 
